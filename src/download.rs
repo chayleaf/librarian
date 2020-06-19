868b0a30a1a6f9c3ee8aa6deda03dbc5964b948f
pavlukivan <ivanpavluk00@gmail.com>
2020-06-20 05:27:33 +0700

    0.2.0; tests

--- src/download.rs
+++ src/download.rs
@@ -9,8 +9,10 @@ use {
     },
     std::{
         env,
-        fs::File,
-        io::copy,
+        error,
+        fmt,
+        fs,
+        io,
         path::{
             Path,
             PathBuf,
@@ -27,62 +29,224 @@ fn url_fname(url: &Url) -> Option<&str> {
         .and_then(|name| if name.is_empty() { None } else { Some(name) })
 }
 
-/// Download file to the provided directory and return the file's location
-pub async fn download_or_find_file_to(url: &str, out_dir: &Path) -> PathBuf {
-    let url_parsed = Url::parse(url).expect("Invalid URL");
-    let fname = url_fname(&url_parsed).expect("Unknown filename");
-    let response = reqwest::get(url).await.expect("Failed to download file");
-        
-    let path = PathBuf::from(out_dir).join(fname);
-    if !path.exists() {
-        let mut dest = File::create(path.clone()).expect("Failed to create file");
-        let content = response.bytes().await.expect("Failed to get response bytes");
-        let mut bytes = content.bytes();
-        copy(&mut bytes, &mut dest).unwrap();
+/// A type specifying an error that occured during downloading a file
+#[derive(Debug)]
+pub enum DownloadError {
+    /// Get request failed
+    RequestError(reqwest::Error),
+    /// Invalid URL
+    ParseError(url::ParseError),
+    /// Couldn't deduce filename from the URL
+    NoFileNameError,
+    /// Couldn't save the fetched file
+    SaveError(io::Error),
+}
+
+impl From<url::ParseError> for DownloadError {
+    #[inline]
+    fn from(err: url::ParseError) -> DownloadError {
+        DownloadError::ParseError(err)
     }
-    path
 }
 
-/// Download file to the build script output directory and return the file's location
-pub async fn download_or_find_file(url: &str) -> PathBuf {
-    let out_dir = env::var("OUT_DIR").expect("This function can only be used in a build script! Consider using download_or_find_file_with_path instead.");
-    let path = Path::new(out_dir.as_str());
-    download_or_find_file_to(url, path).await
+impl From<reqwest::Error> for DownloadError {
+    #[inline]
+    fn from(err: reqwest::Error) -> DownloadError {
+        DownloadError::RequestError(err)
+    }
 }
 
-/// Extract the archive to the provided directory
-pub fn extract_archive_to(archive: &Path, dir: &Path) {
-    if archive.file_name().unwrap().to_string_lossy().ends_with(".zip") {
-        let zipfile = File::open(archive).unwrap();
-        let reader = zipfile.read_zip().expect("Failed to read zip");
+impl From<io::Error> for DownloadError {
+    #[inline]
+    fn from(err: io::Error) -> DownloadError {
+        DownloadError::SaveError(err)
+    }
+}
+
+impl fmt::Display for DownloadError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use DownloadError::*;
+        match *self {
+            NoFileNameError => write!(f, "couldn't infer file name from the url"),
+            RequestError(ref e) => e.fmt(f),
+            ParseError(ref e) => e.fmt(f),
+            SaveError(ref e) => e.fmt(f),
+        }
+    }
+}
+
+impl error::Error for DownloadError {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        use DownloadError::*;
+        match *self {
+            NoFileNameError => None,
+            RequestError(ref e) => Some(e),
+            ParseError(ref e) => Some(e),
+            SaveError(ref e) => Some(e),
+        }
+    }
+}
+
+/// Download file if it doesn't already exist, and return the file's location.
+/// 
+/// You can use `out_dir` to specify the download directory, otherwise the build script output directory will be used.
+/// 
+/// Example:
+/// ```
+/// # fn run() -> Result<(), Box<dyn std::error::Error>> {
+/// librarian::download_or_find_file("https://example.com/file.zip", None)?;
+/// # Ok(())
+/// # }
+/// ```
+pub fn download_or_find_file(
+    url: &str,
+    out_dir: Option<&Path>
+) -> Result<PathBuf, DownloadError> {
+    use DownloadError::*;
+
+    let url_parsed = Url::parse(url)?;
+    let fname = url_fname(&url_parsed);
+    if let Some(fname) = fname {
+        let out_dir = if let Some(out_dir) = out_dir {
+            PathBuf::from(out_dir)
+        } else {
+            let out_dir = env::var("OUT_DIR").expect("You must provide the output directory when not running from a build script.");
+            PathBuf::from(out_dir)
+        };
+        let path = out_dir.join(fname);
+        if !path.exists() {
+            let response = reqwest::blocking::get(url)?;
+            let mut dest = fs::File::create(path.clone())?;
+            let content = response.bytes()?;
+            let mut bytes = content.bytes();
+            io::copy(&mut bytes, &mut dest)?;
+        }
+        Ok(path)
+    } else {
+        Err(NoFileNameError)
+    }
+}
+
+/// A type specifying an error that occured during an archive extraction
+#[derive(Debug)]
+pub enum ExtractError {
+    /// Failed to read the zip file
+    ZipError(rc_zip::Error),
+    /// Failed to save files from the archive to the HDD
+    WriteError(io::Error),
+}
+
+impl From<rc_zip::Error> for ExtractError {
+    #[inline]
+    fn from(err: rc_zip::Error) -> ExtractError {
+        ExtractError::ZipError(err)
+    }
+}
+
+impl From<io::Error> for ExtractError {
+    #[inline]
+    fn from(err: io::Error) -> ExtractError {
+        ExtractError::WriteError(err)
+    }
+}
+
+impl fmt::Display for ExtractError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use ExtractError::*;
+        match *self {
+            ZipError(ref e) => e.fmt(f),
+            WriteError(ref e) => e.fmt(f),
+        }
+    }
+}
+
+impl error::Error for ExtractError {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        use ExtractError::*;
+        match *self {
+            ZipError(ref e) => Some(e),
+            WriteError(ref e) => Some(e),
+        }
+    }
+}
+
+/// Extract the archive and return the path to the extracted files. **Only zip archives are supported for now.**
+pub fn extract_archive<T: AsRef<Path> + ?Sized>(
+    archive: &T,
+    target: Option<&Path>
+) -> Result<PathBuf, ExtractError> {
+    let target = if let Some(target) = target {
+        PathBuf::from(target)
+    } else {
+        PathBuf::from(env::var("OUT_DIR").expect("You must provide the output directory when not running from a build script."))
+    };
+    
+    if archive.as_ref().file_name().unwrap().to_string_lossy().ends_with(".zip") {
+        let zipfile = fs::File::open(archive)?;
+        let reader = zipfile.read_zip()?;
         for entry in reader.entries() {
-            let dir = PathBuf::from(dir);
             match entry.contents() {
                 EntryContents::Directory(c) => {
-                    let path = dir.join(c.entry.name());
-                    std::fs::create_dir_all(path.parent().unwrap()).expect("Failed to create dir");
+                    let path = target.join(c.entry.name());
+                    fs::create_dir_all(path.parent().unwrap())?;
                 },
                 EntryContents::File(c) => {
-                    let path = dir.join(c.entry.name());
-                    std::fs::create_dir_all(path.parent().unwrap()).expect("Failed to create dir");
-                    let mut entry_writer = File::create(path).expect("Failed to create path");
-                    let mut entry_reader = c
+                    let path = target.join(c.entry.name());
+                    fs::create_dir_all(path.parent().unwrap())?;
+                    let mut writer = fs::File::create(path)?;
+                    let mut reader = c
                         .entry
                         .reader(|offset| positioned_io::Cursor::new_pos(&zipfile, offset));
 
-                    copy(&mut entry_reader, &mut entry_writer).expect("Copy from zip file failed");
+                    io::copy(&mut reader, &mut writer)?;
                 },
+                // Symlinks aren't supported! Open an issue if you need them.
                 _ => {}
             }
         }
     } else {
         panic!("archive format not supported yet");
     }
+    Ok(target)
 }
 
-/// Extract the archive to the build script output directory and return the said directory location
-pub fn extract_archive(archive: &Path) -> PathBuf {
-    let path = PathBuf::from(env::var("OUT_DIR").expect("This function can only be used in a build script! Consider using extract_archive_to instead."));
-    extract_archive_to(archive, &path);
-    path
+#[cfg(test)]
+mod download_tests {
+    #[test]
+    fn unzip() {
+        use crate::tests::dir_list_equals;
+        use crate::*;
+        let cur_file = Path::new(file!());
+        let root = cur_file.parent().unwrap().parent().unwrap();
+        let out = root.join("target").join("test").join("unzip");
+        let _ = fs::remove_dir_all(out.as_path());
+        let data_dir = root.join("test_input");
+        assert_eq!(extract_archive(data_dir.join("file.zip").as_path(), Some(out.as_path())).unwrap(), out);
+        assert_eq!(true, dir_list_equals(out.join("zip").as_path(), vec![ "compressed.txt" ]));
+        assert_eq!(
+            fs::read_to_string(out.join("zip").join("compressed.txt")).unwrap(),
+            "it works!".to_string()
+        );
+    }
+
+    #[test]
+    fn web() {
+        use crate::tests::dir_list_equals;
+        use crate::*;
+        let cur_file = Path::new(file!());
+        let root = cur_file.parent().unwrap().parent().unwrap();
+        let out = root.join("target").join("test").join("web");
+        let _ = fs::remove_dir_all(out.as_path());
+        let _ = fs::create_dir_all(out.as_path());
+        let url = "https://httpbin.org/base64/YWJj";
+        let out_expect = out.join("YWJj");
+        assert_eq!(download_or_find_file(url, Some(out.as_path())).unwrap(), out_expect);
+        assert_eq!(true, dir_list_equals(out.as_path(), vec![ "YWJj" ]));
+        assert_eq!(
+            fs::read_to_string(out_expect.as_path()).unwrap(),
+            "abc".to_string()
+        );
+        let url = "http://invalid.url/but/the/file/is/still/cached/YWJj";
+        assert_eq!(download_or_find_file(url, Some(out.as_path())).unwrap(), out_expect);
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,8 @@
 
 use std::{
     env,
-    fs::{copy, read_dir},
+    fs,
+    io,
     path::{Path, PathBuf},
 };
 
@@ -14,75 +15,152 @@ mod download;
 #[cfg(feature = "download")]
 pub use download::*;
 
-/// Get assumed path to the target executable directory (only works from a build script)
-fn get_target_dir() -> PathBuf {
+/// Get assumed path to the target executable directory. Only works from build scripts.
+fn get_target_dir() -> io::Result<PathBuf>  {
     // Please tell me there's a better way... please...
-    let cur_exe = env::current_exe().unwrap();
-    cur_exe.parent().unwrap().parent().unwrap().parent().unwrap().to_path_buf()
+    let cur_exe = env::current_exe()?;
+    Ok(cur_exe.parent().unwrap().parent().unwrap().parent().unwrap().to_path_buf())
 }
 
-/// Get dynamic lib extension (.dll on windows, .so otherwise)
-fn get_dylib_extension() -> &'static str {
-    if cfg!(windows) {
+/// Get dynamic lib extension (.dll for windows targets, .so otherwise). Only works from build scripts.
+fn get_dylib_extension() -> Result<&'static str, env::VarError> {
+    let target = env::var("TARGET")?;
+    Ok(if target.contains("pc-windows") {
         "dll"
     } else {
         "so"
-    }
+    })
 }
 
-/// Install a single dynamic library with the given name and extension from the given path to the provided directory
-pub fn install_dylib_with_extension_to(from: &Path, dylib_name: &str, extension: &str, target_dir: &Path) {
-    let filename = dylib_name.to_string() + "." + extension;
-    let path = from.join(filename.clone());
-    if !path.exists() {
-        panic!("Provided dylib doesn't exist!");
-    }
-    copy(path, target_dir.join(filename)).expect("Failed to copy dylib");
+/// Dynamic library filter used to specify which library files needs to be copied.
+#[derive(Debug)]
+pub enum DyLibNameFilter<'a> {
+    /// Filename must match the string (Example: `"SDL2.dll"`)
+    FileName(&'a str),
+    /// Extension must match the string (Example: `"dll"`)
+    Extension(&'a str),
+    /// Library name must match the string (Example: `"SDL2"`).
+    /// Extension will be inferred from the target platform.
+    /// Files with an additional "lib" prefix will match as well.
+    LibName(&'a str),
 }
 
-/// Install a single dynamic library with the given name from the given path to the provided directory
-pub fn install_dylib_to(from: &Path, dylib_name: &str, target_dir: &Path) {
-    install_dylib_with_extension_to(from, dylib_name, get_dylib_extension(), target_dir)
-}
+/// Install all dynamic libs from a directory to the target directory.
+/// 
+/// The `dylib` argument can be used to specify the criteria a file needs to match to be installed (See [DyLibNameFilter](DyLibNameFilter) docs).
+/// Default behavior is to install every library with an extension that matches the target platform's dylib extension.
+/// `target_dir` can be left empty to attempt to automatically find the target executable directory.
+/// 
+/// To just install all of the dynamic libraries from a folder, do:
+/// ```
+/// # fn run() -> std::io::Result<()> {
+/// # let path_to_dylib_folder = std::path::Path::new(".");
+/// librarian::install_dylibs(path_to_dylib_folder, None, None)?;
+/// // The application should now have all the dynamic libraries in the same folder as the executable
+/// # Ok(())
+/// # }
+/// ```
+pub fn install_dylibs<T: AsRef<Path> + ?Sized>(
+    from: &T,
+    filter: Option<DyLibNameFilter<'_>>,
+    target_dir: Option<&Path>,
+) -> io::Result<()> {
+    use DyLibNameFilter::*;
 
-/// Install a single dynamic library with the given name from the given path to the target executable directory
-pub fn install_dylib(from: &Path, dylib_name: &str) {
-    install_dylib_to(from, dylib_name, get_target_dir().as_path())
-}
+    let extension = if let Some(Extension(extension)) = filter {
+        ".".to_string() + extension
+    } else if let Some(FileName(_)) = filter {
+        String::new()
+    } else {
+        get_dylib_extension().expect("Couldn't detect dylib extension").to_string()
+    };
 
-/// Install all dynamic libs with given extension from the given path to the provided directory
-pub fn install_dylibs_with_extension_to(from: &Path, extension: &str, target_dir: &Path) {
-    let extension = ".".to_string() + extension;
-    for entry in read_dir(from).expect("Can't read dylib source dir")  {
-        let entry_path = entry.expect("Invalid fs entry").path();
+    let target_dir = if let Some(target_dir) = target_dir {
+        PathBuf::from(target_dir)
+    } else {
+        get_target_dir()?
+    };
+
+    for entry in fs::read_dir(from)?  {
+        let entry_path = entry?.path();
         if let Some(file_name) = entry_path.file_name() {
-            let file_name = file_name.to_str().unwrap();
-            if file_name.ends_with(&extension) {
-                copy(&entry_path, target_dir.join(file_name)).expect("Failed to copy dylib");
+            if let Some(file_name) = file_name.to_str() {
+                let matches = if let Some(FileName(target_fname)) = filter {
+                    target_fname == file_name
+                } else if let Some(Extension(_)) = filter {
+                    file_name.ends_with(extension.as_str())
+                } else if let Some(LibName(lib_name)) = filter {
+                    file_name == lib_name.to_string() + &extension || file_name == "lib".to_string() + lib_name + &extension
+                } else {
+                    file_name.ends_with(extension.as_str())
+                };
+
+                if matches {
+                    fs::copy(entry_path.as_path(), target_dir.join(file_name).as_path())?;
+                }
             }
         }
     }
-}
-
-/// Install all dynamic libs from the given path to the provided directory
-pub fn install_dylibs_to(from: &Path, target_dir: &Path) {
-    install_dylibs_with_extension_to(from, get_dylib_extension(), target_dir)
-}  
-
-/// Install all dynamic libs from the given path to the target executable directory
-pub fn install_dylibs(from: &Path) {
-    install_dylibs_to(from, get_target_dir().as_path())
+    Ok(())
 }
 
 /// Add a cargo link search path (only works strictly from a build script)
-pub fn add_link_search_path(path: &Path) {
-    println!("cargo:rustc-link-search=all={}", path.display());
+/// 
+/// The function can be considered an analog of `install_dylibs` for static libs; it makes the static libs in a folder available to the linker.
+/// 
+/// Usage:
+/// ```
+/// # fn run() {
+/// # let path_to_static_lib_folder = std::path::Path::new(".");
+/// librarian::add_link_search_path(path_to_static_lib_folder);
+/// # }
+/// ```
+pub fn add_link_search_path<T: AsRef<Path> + ?Sized>(path: &T) {
+    println!("cargo:rustc-link-search=all={}", path.as_ref().display());
 }
 
 #[cfg(test)]
 mod tests {
+    use std::{
+        collections::HashMap,
+        fs,
+        path::Path,
+        vec::Vec,
+    };
+
+    pub(crate) fn dir_list_equals(path: &Path, list: Vec<&'static str>) -> bool {
+        let mut results = HashMap::new();
+        for entry in fs::read_dir(path).unwrap() {
+            let entry_path = entry.unwrap().path();
+            let file_name = entry_path.file_name().unwrap().to_str().unwrap();
+            *results.entry(file_name.to_string()).or_insert(0) += 1;
+        }
+        list.len() == results.len() && list.iter().all(|&x| *results.entry(x.to_string()).or_insert(0) == 1)
+    }
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn install_dylibs_test() {
+        let cur_file = Path::new(file!());
+        let root = cur_file.parent().unwrap().parent().unwrap();
+        // For some reason, you can't just receive a temporary directory from cargo, you gotta manage it yourself
+        let out = root.join("target").join("test").join("install_dylibs");
+        let _ = fs::remove_dir_all(out.as_path());
+        let data_dir = root.join("test_input");
+        let dll_out = out.join("dll");
+        let so_out = out.join("so");
+        let fn_out = out.join("fn");
+        fs::create_dir_all(dll_out.as_path()).unwrap();
+        fs::create_dir_all(so_out.as_path()).unwrap();
+        fs::create_dir_all(fn_out.as_path()).unwrap();
+
+        use crate::*;
+        use crate::DyLibNameFilter::*;
+        install_dylibs(data_dir.as_path(), Some(Extension("dll")), Some(dll_out.as_path())).unwrap();
+        install_dylibs(data_dir.as_path(), Some(Extension("so")), Some(so_out.as_path())).unwrap();
+        install_dylibs(data_dir.as_path(), Some(FileName("dummy")), Some(fn_out.as_path())).unwrap();
+
+        assert_eq!(true, dir_list_equals(dll_out.as_path(), vec![ "dummy0.dll", "dummy1.dll" ]));
+        assert_eq!(true, dir_list_equals(so_out.as_path(), vec![ "dummy.so", "libdummy.so" ]));
+        assert_eq!(true, dir_list_equals(fn_out.as_path(), vec![ "dummy" ]));
     }
 }
